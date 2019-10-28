3cc1aa9b14b0127e6342f56dcb40f6cbc64c7d1f
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-10-28 13:16:17 +0100

    Uebung 3 loesung

--- src/main/java/uebung3/Cow.java
+++ src/main/java/uebung3/Cow.java
@@ -2,14 +2,12 @@ package uebung3;
 
 class Cow {
     private String name ;
-
     static private int cowCounter = 0 ;
 
     public Cow(String name) {
         this.name = name ;
         cowCounter++ ;
     }
-
 
     public String getName() {
         return name ;

fdcf11a02abc45e74f17046191c12b70a4d178c1
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-11-11 15:37:18 +0100

    uebung5 Vorlage

--- src/main/java/uebung5/MyGridworld.java
+++ src/main/java/uebung5/MyGridworld.java
@@ -1,14 +1,12 @@
 package uebung5;
 
-import gridworld.framework.actor.ActorWorld;
 
 public class MyGridworld{
 
     public static void main(String[] args) {
-        ActorWorld world = new ActorWorld();
 
-        world.add(new Breeder());
-        world.show();
+        // Benutze die Klasse "BugRunner" als Vorlage.
+        // Die befindet sich im folgenden package: "gridworld/firstProject/"
 
     }
 

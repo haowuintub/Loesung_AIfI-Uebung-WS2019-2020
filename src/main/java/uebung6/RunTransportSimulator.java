bb9337e735638e85bb2dd698feff644a570a1e65
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-11-19 17:38:31 +0100

    Vorlage Uebung6

---
+++ src/main/java/uebung6/RunTransportSimulator.java
@@ -0,0 +1,9 @@
+package uebung6;
+
+public class RunTransportSimulator {
+
+    public static void main(String[] args) {
+        // This is where you will create instances of all your movable classes. You can then test their functionality.
+    }
+
+}

8171ed43613d7bc63b2836324f8d420be1cf1b81
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-11-26 17:42:15 +0100

    Uebung7 Loesung

--- src/main/java/uebung7/Person.java
+++ src/main/java/uebung7/Person.java
@@ -4,14 +4,23 @@ class Person {
 
     private String name ;
     private int age ;
+    private boolean hungry;
 
     Person(String name, int age) {
         this.name = name;
         this.age = age;
+        this.hungry = true ;
     }
 
     public String getName() {
         return name;
+    }
+
+    boolean getHungry() {
+        return hungry;
+    }
+    void setHungry(boolean hungry) {
+        this.hungry = hungry ;
     }
 
     void celebrateBirthday() {

401484861ca2af4aab269d8a2889d8fe6fc9a598
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-12-02 13:33:33 +0100

    Uebung8 Vorlage : BabyBug

---
+++ src/main/java/uebung8/DisposableBug.java
@@ -0,0 +1,31 @@
+package uebung8;
+
+import gridworld.framework.actor.Bug;
+
+import java.awt.*;
+
+class DisposableBug extends Bug {
+
+    private int steps = 1 ;
+
+    DisposableBug() {
+        super(Color.BLUE);
+    }
+
+    @Override
+    public void act() {
+
+        super.act();
+
+        if (Math.random() <= 0.4) {
+            this.turn();
+        }
+
+        if (Math.random() <= 0.8 && steps > 25) {
+            this.removeSelfFromGrid();
+        }
+
+        steps ++ ;
+    }
+
+}

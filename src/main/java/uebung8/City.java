3ca90be03a39d49a848b5e6407b6fae12163218c
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-12-03 14:35:15 +0100

    Uebung8 Loesung (Cherrypick): City und RunCities

---
+++ src/main/java/uebung8/City.java
@@ -0,0 +1,20 @@
+package uebung8;
+
+public class City {
+
+    private String name ;
+    private String country ;
+    private int population ;
+
+    City(String name, String country, int population) {
+        this.name = name;
+        this.country = country;
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "" ; // Fill this out!
+    }
+
+}

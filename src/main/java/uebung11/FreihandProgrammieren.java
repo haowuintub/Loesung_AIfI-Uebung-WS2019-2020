9729cf2a5cb8da7770fc079f37d56ece43a039f6
Jakob Rehmann <jakob.rehmann@gmail.com>
2020-01-07 15:05:32 +0100

    Freihand Programmierung Loesung

--- src/main/java/uebung11/FreihandProgrammieren.java
+++ src/main/java/uebung11/FreihandProgrammieren.java
@@ -3,7 +3,7 @@ package uebung11;
 import java.util.Arrays;
 import java.util.List;
 
-public class FreihandProgrammieren {
+class FreihandProgrammieren {
 
     public static void main(String[] args) {
         List<Double> myNumbers = Arrays.asList(2.3, 5.0, 1.2, -3.4, Math.PI, 0.0);
@@ -13,12 +13,14 @@ public class FreihandProgrammieren {
 
     private static String mean(List<Double> myNumbers) {
         if (myNumbers.isEmpty()) {
-            return "NAN";
+            return "List is Empty";
         }
+
         Double sum = 0.;
         for (Double num : myNumbers) {
             sum += num;
         }
+
         return sum.toString() ;
     }
 }

21ca3e55e3bc2b9b2ddd850825cdee47ed930f74
Jakob Rehmann <jakob.rehmann@gmail.com>
2020-01-07 17:16:28 +0100

    Loesung Update

--- src/main/java/uebung11/YouHaveNoMoneyException.java
+++ src/main/java/uebung11/YouHaveNoMoneyException.java
@@ -2,4 +2,8 @@ package uebung11;
 
 class YouHaveNoMoneyException extends Exception {
 
+    YouHaveNoMoneyException() {
+        super("Unfortunately, Jakob, you do not have enough money on your Account and we do not trust you enought, to let you have a negative balance. Sorry!");
+    }
+
 }
